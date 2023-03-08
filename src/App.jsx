971807ce0cff415961428b5e00e66dacdcc7f301
Shiwani <shiwanisahu34@gmail.com>
2023-03-08 22:55:08 +0530

    user-bot-post

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
 import { useState } from 'react'
+import send from  "./assets/send.svg";
+import user from "./assets/user.png";
+import logo from "./assets/logo.png";
+import bot from "./assets/bot.png";
+import loadingIcon from "./assets/loader.svg";
 
-
-
-function App() {
+let arr = [
+  {type: "user", post: "fafafafaffaf"},
+  {type: "bot",post: "fafaffaffaf"},
+  ];
   
-  return (
-   <div>
-    App is working
-    </div>
+function App() {
+  const [input , setInput] = useState("");
+  const [posts , setPosts] = useState(arr);
+
+  return (  
+    <main className ="chatGPT-app">
+    <section className="chat-container">
+      <div className="layout">
+
+      {posts.map((post , index) => (
+        <div
+        key ={index}
+        className= {`chat-bubble ${post.type ==="bot" || post.type === "loading" ? "bot" : ""}`}>
+        <div className= "avatar">
+          <img src = {post.type ==="bot" || post.type === "loading" ? "bot" : user} />
+        </div>
+
+        {post.type === "loading" ? (
+          <div className="loader">
+          <img src= {loadingIcon} />
+        </div>
+        ) : (
+          <div className= "post">{post.post}</div>
+        )}
+        
+        </div>
+      ))}
+        </div>
+
+    </section>
+    <footer>
+
+      <input
+      className= 'composebar'
+      autoFocus
+      type="text"
+      placeholder ="Ask Anything..!!"
+      onChange={() => {}}
+      />
+
+      <div className="send-button">
+      <img src ={send} />
+      </div>
+    </footer>
+   </main>
   )
 }
 
-export default App
+export default App;
